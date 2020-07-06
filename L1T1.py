729cdb7c9a3e16fbc7494011174338bc850a93d3
comradsnarky <nick440.np@gmail.com>
2020-07-07 01:19:14 +0300

    lesson_1, lask_1
    Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.

---
+++ L1T1.py
@@ -0,0 +1,9 @@
+name = input('Enter your name: ')
+lName = input('Enter your last name: ')
+login = input('Enter your login:')
+password = input('Enter your password: ')
+print(f'\nYou entered following information:\n'
+      f'Your name is {name};\n'
+      f'Your last name is {lName};\n'
+      f'Your login is {login};\n'
+      f'Your password is {password}.')
