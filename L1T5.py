a8eb26bb6e4787e529df4ac583c2062a46df8259
comradsnarky <nick440.np@gmail.com>
2020-07-07 12:47:29 +0300

    lesson_1, task_5
    Запросите у пользователя значения выручки и издержек фирмы. Определите, с каким финансовым результатом работает фирма (прибыль — выручка больше издержек, или убыток — издержки больше выручки). Выведите соответствующее сообщение. Если фирма отработала с прибылью, вычислите рентабельность выручки (соотношение прибыли к выручке). Далее запросите численность сотрудников фирмы и определите прибыль фирмы в расчете на одного сотрудника.

--- L1T5.py
+++ L1T5.py
@@ -6,7 +6,10 @@ if income > expenditure:
     print(f'\nCongratulations, you have a profitable business!\n'
           f'Your company\'s profitability equals {profitability}%\n')
     stuff = int(input('Enter the number of company\'s employees: '))
-    print(f'Company\'s profitability by employee equals '
+    print(f'\nCompany\'s profitability by employee equals '
           f'{round((profitability / stuff), 2)}% per employee')
+elif income == expenditure:
+    print('\nThere\'s no profit in your company. You\'re barely '
+          'making ends meet.')
 else:
     print('\nUnfortunately, your company is not doing very well.')
