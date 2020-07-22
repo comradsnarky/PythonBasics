fdbfd2d06ba5e36ddf4b16d884cc515cff17f2ee
comradsnarky <nick440.np@gmail.com>
2020-07-23 00:19:31 +0300

    lesson_5, task_4
    Создать (не программно) текстовый файл со следующим содержимым:
    One — 1
    Two — 2
    Three — 3
    Four — 4
    Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные. При этом английские числительные должны заменяться на русские. Новый блок строк должен записываться в новый текстовый файл.

---
+++ L5T4.py
@@ -0,0 +1,17 @@
+from googletrans import Translator
+translator = Translator(service_urls=['translate.google.com'])
+
+my_f1 = open('text_4.txt', 'r', encoding='utf-8')
+content = my_f1.readlines()
+
+my_f2 = open('text_4.1.txt', 'w', encoding='utf-8')
+
+
+my_dict = []
+for i in range(len(content)):
+    _ = content[i].split()
+    dictionary = translator.translate(_[0], dest='ru')
+    my_f2.write(f'{dictionary.text.title()} - {_[2]}\n')
+
+my_f1.close()
+my_f2.close()
